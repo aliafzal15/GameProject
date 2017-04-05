c32d447b66baf670f6fc32d12492119bafb88878
aliafzal15 <10bicseaafzal@seecs.edu.pk>
2017-04-06 00:35:31 +0500

    Commiting Code for WeaponDecorator

---
+++ Gradle-Android-Libgdx/tmp_Ali&Arad/Ali/WeaponDecorator.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+/**
+ * This Class is for the WeaponDecorator
+ * 
+ * 	@author Ali Afzal
+ */
+
+public abstract class WeaponDecorator extends WeaponSpecialEnchantment {
+
+	private WeaponSpecialEnchantment weaponSpecialEnchantment;
+	
+	/**
+	 * This is the parameterized constructor
+	 * @param weaponSpecialEnchantment
+	 */
+	public WeaponDecorator(WeaponSpecialEnchantment weaponSpecialEnchantment){
+		
+		this.weaponSpecialEnchantment=weaponSpecialEnchantment;
+	}
+	
+	/**
+	 * This function set the stacks of enchantment values and enchantment type
+	 * 
+	 */
+	public Stack getEnchantment() {		
+		return this.enchantmentValueStack;
+	}
+	
+}
